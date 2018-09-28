e8d2e25c7f1ee12db738aaf6a043d239b3d04b52
Andrew <a.muncey@chester.ac.uk>
2018-09-28 10:06:31 +0100

    persist dragging of items, implement swipe left to delete

--- app/src/main/java/com/tinyappco/shoplist/MainActivity.kt
+++ app/src/main/java/com/tinyappco/shoplist/MainActivity.kt
@@ -115,16 +115,19 @@ class MainActivity : AppCompatActivity() {
     inner class DragCallback : ItemTouchHelper.Callback() {
         override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int {
             val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
-            return makeMovementFlags(dragFlags, 0)
+            return makeMovementFlags(dragFlags, ItemTouchHelper.LEFT)
         }
 
         override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+            val movedItem = adapter.list.removeAt(viewHolder.adapterPosition)
+            adapter.list.add(target.adapterPosition, movedItem)
             adapter.notifyItemMoved(viewHolder.adapterPosition,target.adapterPosition)
             return true
         }
 
         override fun onSwiped(recyclerView: RecyclerView.ViewHolder, direction: Int) {
-
+            adapter.list.removeAt(recyclerView.adapterPosition)
+            adapter.notifyItemRemoved(recyclerView.adapterPosition)
         }
     }
 }
